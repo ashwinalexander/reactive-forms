81afd5cea22f2580a5afad56cac73328ad90f479
ashwin alexander <alexwin75@gmail.com>
2021-09-04 23:26:48 -0400

    async validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { normalizeGenFileSuffix } from '@angular/compiler/src/aot/util';
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -22,7 +23,7 @@ export class AppComponent implements OnInit {
     this.signUpForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl("ashwin@hauskey.com", [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
       })
       ,
       'gender': new FormControl('male'),
@@ -53,6 +54,23 @@ export class AppComponent implements OnInit {
     return null;
   }
 
+
+  //async custom validator
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ 'emailsIsForbidden': true })
+        }
+        else {
+          resolve(null);
+        }
+      }, 1500);
+    }
+    );
+
+    return promise;
+  }
 
 
 
