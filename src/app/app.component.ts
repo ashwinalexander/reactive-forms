37d454fbb9a4a499de920a6876d55fd776ed3a94
ashwin alexander <alexwin75@gmail.com>
2021-09-04 20:35:13 -0400

    adding form control arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -17,14 +17,23 @@ export class AppComponent implements OnInit {
 
     //controls are key value pairs
     this.signUpForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      'email': new FormControl("ashwin@hauskey.com", [Validators.required, Validators.email]),
-      'gender': new FormControl('male')
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl("ashwin@hauskey.com", [Validators.required, Validators.email])
+      })
+      ,
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
 
     });
 
 
 
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
   }
 
   onSubmit() {
