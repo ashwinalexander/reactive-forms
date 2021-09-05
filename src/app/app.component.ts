d3f09c231f107e9dc78a6d4ebdaa16df83c1c4b2
ashwin alexander <alexwin75@gmail.com>
2021-09-04 20:11:31 -0400

    getting access to controls in reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+
+  signUpForm: FormGroup;
+
+
+  ngOnInit() {
+
+
+    //controls are key value pairs
+    this.signUpForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl("ashwin@hauskey.com", [Validators.required, Validators.email]),
+      'gender': new FormControl('male')
+
+    });
+
+
+
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
+
+
+
+
+
+
 }
